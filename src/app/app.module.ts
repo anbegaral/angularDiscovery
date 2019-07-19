95dcedf44fd3de1096bae6699314677167cc1456
anbegaral <anab.garciauk@gmail.com>
2019-07-19 21:05:43 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { TranslateService, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { SQLite } from '@ionic-native/sqlite/ngx';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { IonicStorageModule } from '@ionic/storage';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -8,14 +12,50 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
+  }
+
+export const firebaseConfig = {
+    apiKey: 'AIzaSyB0uWbXfQ3QauZzCoJZ8DjZi-PFkYEOYTQ',
+    authDomain: 'discoveryag-15cc2.firebaseapp.com',
+    databaseURL: 'https://discoveryag-15cc2.firebaseio.com',
+    projectId: 'discoveryag-15cc2',
+    storageBucket: 'discoveryag-15cc2.appspot.com',
+    messagingSenderId: '79539419393'
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+      BrowserModule,
+      IonicModule.forRoot(),
+      IonicStorageModule.forRoot({
+        name: '__mydb',
+           driverOrder: ['sqlite', 'websql', 'indexeddb']
+      }),
+      AppRoutingModule,
+      AngularFireDatabaseModule,
+      AngularFireModule.initializeApp(firebaseConfig),
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      }),
+      HttpClientModule
+    ],
   providers: [
     StatusBar,
     SplashScreen,
+    TranslateService,
+    SQLite,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
